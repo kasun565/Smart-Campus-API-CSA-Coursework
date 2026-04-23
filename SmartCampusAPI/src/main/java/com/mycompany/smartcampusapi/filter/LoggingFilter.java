ac92a5cdcaf3baf8d6131d8282d7ac52e96dd7dc
kasun565 <kasunkanchana56@gmail.com>
2026-04-24 00:39:29 +0530

    Create LoggingFilter.java

---
+++ SmartCampusAPI/src/main/java/com/mycompany/smartcampusapi/filter/LoggingFilter.java
@@ -0,0 +1,31 @@
+package com.mycompany.smartcampusapi.filter;
+
+import jakarta.ws.rs.container.ContainerRequestContext;
+import jakarta.ws.rs.container.ContainerRequestFilter;
+import jakarta.ws.rs.container.ContainerResponseContext;
+import jakarta.ws.rs.container.ContainerResponseFilter;
+import jakarta.ws.rs.ext.Provider;
+import java.io.IOException;
+import java.util.logging.Logger;
+
+@Provider
+public class LoggingFilter implements ContainerRequestFilter, ContainerResponseFilter {
+    
+    private static final Logger LOGGER = Logger.getLogger(LoggingFilter.class.getName());
+    
+    @Override
+    public void filter(ContainerRequestContext requestContext) throws IOException {
+        LOGGER.info(String.format("→ REQUEST: %s %s", 
+            requestContext.getMethod(), 
+            requestContext.getUriInfo().getRequestUri()));
+    }
+    
+    @Override
+    public void filter(ContainerRequestContext requestContext, 
+                       ContainerResponseContext responseContext) throws IOException {
+        LOGGER.info(String.format("← RESPONSE: %s %s - Status: %d", 
+            requestContext.getMethod(),
+            requestContext.getUriInfo().getRequestUri(),
+            responseContext.getStatus()));
+    }
+}
